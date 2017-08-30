9375c8987d0b7d0a04b73043927e94d6a0d3e086
ssaita <saita@kct.ac.jp>
2017-08-30 23:41:31 +0900

    Committed for the first time.

---
+++ paramt.c
@@ -0,0 +1,23 @@
+//
+//  paramt.c
+//  Triangx_NK
+//
+//  Created by SaitaSatoko on 2017/08/23.
+//  Copyright © 2017年 SaitaSatoko. All rights reserved.
+//
+
+
+//外部結合の定義
+int iprf=6;
+//     devision order
+int iprm1=32,iprm2=122,iprm3=482,iprm4=1922,iprm5=7682,iprm6=30722,iprm7=122882;
+int iprn2=240,iprn3=960,iprn4=3840,iprn5=15360,iprn6=61440,iprn7=245760;
+int ipro=5;
+int iprv=7682;
+int ipru=15360;
+//     altitude and variables
+int iprz=240;
+int ipri=8;
+int iprc=4;
+
+double pi=3.141592;
